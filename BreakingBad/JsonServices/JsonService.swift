5ef3d31a70aba9cca9514be9741037c6bf8f1ed7
Kevin <kevinandrewreid@aol.com>
2020-12-11 10:12:26 +0000

    updated

--- BreakingBad/JsonServices/JsonService.swift
+++ BreakingBad/JsonServices/JsonService.swift
@@ -11,12 +11,16 @@ public class JsonService: NSObject {
     
     public func getBreakingBadCharacters(completion: @escaping (Bool, Any?, Error?) -> Void)  {
         guard let url = URL(string: "https://breakingbadapi.com/api/characters") else { return}
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
         let session = URLSession.shared
-        let task = session.dataTask(with: url) { ( data, _, _) in
+        let task = session.dataTask(with:  urlRequest) { ( data, _, _) in
             guard let data = data else { return }
             do {
                 let details = try JSONDecoder().decode([Details].self, from: data)
-            
                 DispatchQueue.main.async {
                     completion(true, details, nil)
                 }
